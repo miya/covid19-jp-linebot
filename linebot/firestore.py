d97f7c9c93aff6b69f327e4bd52f40ba0be2d76f
0x0u <m0zurillex@gmail.com>
2020-06-18 10:29:26 +0900

    refactor: docstringを簡略化した

--- linebot/firestore.py
+++ linebot/firestore.py
@@ -12,160 +12,72 @@ b = "before"
 
 
 def get_update():
-    """
-    Returns
-    -------
-    アップデート時間: str
-    """
+    """アップデート時間"""
     return col_ref.document(n).get().to_dict()["detail"]["update"]
 
 
 def get_total_cases():
-    """
-    Returns
-    -------
-    全国の現在の総感染者数: int
-    """
+    """全国の現在の感染者数"""
     return col_ref.document(n).get().to_dict()["total"]["total_cases"]
 
 
 def get_before_total_cases():
-    """
-    Returns
-    -------
-    全国の前日の総感染者数: int
-    """
+    """全国の前日の感染者数"""
     return col_ref.document(b).get().to_dict()["total"]["total_cases"]
 
 
-def get_total_deaths():
-    """
-    Returns
-    -------
-    全国の現在の総死亡者数: int
-    """
-    return col_ref.document(n).get().to_dict()["total"]["total_deaths"]
-
-
-def get_before_total_deaths():
-    """
-    Returns
-    -------
-    全国の前日の総死亡者数の取得: int
-    """
-    return col_ref.document(b).get().to_dict()["total"]["total_deaths"]
-
-
 def get_pref_cases(pref_name):
-    """
-    Parameters
-    ----------
-    pref_name: str
-        都道府県名
-
-    Returns
-    -------
-    対象の現在の総感染者数: int
-    """
+    """指定した都道府県の現在の感染者数"""
     return col_ref.document(n).get().to_dict()["prefectures"][pref_name]["cases"]
 
 
 def get_before_pref_cases(pref_name):
-    """
-    Parameters
-    ----------
-    pref_name: str
-        都道府県名
-
-    Returns
-    -------
-    対象の前日の総感染者数: int
-    """
+    """指定した都道府県の前日の感染者数"""
     return col_ref.document(b).get().to_dict()["prefectures"][pref_name]["cases"]
 
 
-def get_pref_deaths(pref_name):
-    """
-    Parameters
-    ----------
-    pref_name: str
-        都道府県名
+def get_total_deaths():
+    """全国の現在の死亡者数"""
+    return col_ref.document(n).get().to_dict()["total"]["total_deaths"]
 
-    Returns
-    -------
-    対象の現在の総死亡者数: int
-    """
+
+def get_before_total_deaths():
+    """全国の前日の死亡者数"""
+    return col_ref.document(b).get().to_dict()["total"]["total_deaths"]
+
+
+def get_pref_deaths(pref_name):
+    """指定した都道府県の現在の死亡者数"""
     return col_ref.document(n).get().to_dict()["prefectures"][pref_name]["deaths"]
 
 
 def get_before_pref_deaths(pref_name):
-    """
-    Parameters
-    ----------
-    pref_name: str
-        都道府県名
-
-    Returns
-    -------
-    対象の前日の総死亡者数: int
-    """
+    """指定した都道府県の前日の死亡者数"""
     return col_ref.document(b).get().to_dict()["prefectures"][pref_name]["deaths"]
 
 
 def get_total_pcr():
-    """
-    Returns
-    -------
-    全国のpcr検査数: int
-    """
+    """全国の現在のpcr検査数"""
     return col_ref.document(n).get().to_dict()["total"]["total_pcr"]
 
 
 def get_before_total_pcr():
-    """
-    Returns
-    -------
-    全国の前日のpcr検査数: int
-
-    """
+    """全国の前日のpcr検査数"""
     return col_ref.document(b).get().to_dict()["total"]["total_pcr"]
 
 
 def get_pref_pcr(pref_name):
-    """
-    Parameters
-    ----------
-    pref_name: str
-        都道府県名
-
-    Returns
-    -------
-    対象都市の現在のpcr検査数
-
-    """
+    """指定した都道府県の現在のpcr検査数"""
     return col_ref.document(n).get().to_dict()["prefectures"][pref_name]["pcr"]
 
 
 def get_before_pref_pcr(pref_name):
-    """
-    Parameters
-    ----------
-    pref_name: str
-        都道府県名
-
-    Returns
-    -------
-    対象都市の前日のpcr検査数
-    """
+    """指定した都道府県の前日のpcr検査数"""
     return col_ref.document(b).get().to_dict()["prefectures"][pref_name]["pcr"]
 
 
 def get_top_pref():
-    """
-    Returns
-    -------
-    全国 + 現在の感染者数上位12都市: list
-    """
+    """全国 + 現在の感染者数上位12都市"""
     pref_data = {}
     pref = col_ref.document(n).get().to_dict()["prefectures"]
     [pref_data.update({i: pref[i]["cases"]}) for i in pref]
